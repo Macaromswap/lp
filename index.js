592f17c58c02257490542016995ab6416b515a12
Macaron <164980497+Macaromswap@users.noreply.github.com>
2024-05-11 02:56:23 +0000

    add stable pool

--- index.js
+++ index.js
@@ -15,12 +15,23 @@ async function getlpPrice() {
         const pools = testres.data.data;
         const provider = new ethers.providers.JsonRpcProvider(TEST_RPC); 
         for(const pool of pools) {
-            const {pair_address, tvl} = pool
-            const tokenContract = new ethers.Contract(pair_address, erc20Abi, provider);
-            const total = await tokenContract.totalSupply();
-            const balance = ethers.utils.formatUnits(total)
-            const price = tvl/balance || 0;
-            poolData[pair_address] = price
+            try {
+                const {pair_address, tvl} = pool
+                let lp_address = pair_address
+                if(pair_address == '0x13c6108d73dea14741cbf3bc6617c6738df4f7ae') {
+                    lp_address = '0x0017e3bbc621078916e3db5a26069e456d1f9872'
+                }
+                if(pair_address == '0x035cca67b0671d8e15e881ed0ca72f97dc93c17d') {
+                    lp_address = '0x443ef7a9d505380619d6a367d6808a5f6474e27d'
+                }
+                const tokenContract = new ethers.Contract(lp_address, erc20Abi, provider);
+                const total = await tokenContract.totalSupply();
+                const balance = ethers.utils.formatUnits(total)
+                const price = tvl/balance || 0;
+                poolData[lp_address] = price
+            } catch (error) {
+                console.error(error)
+            }
         }
     }
     const res = await axios.get(INFO_URL)
@@ -28,12 +39,16 @@ async function getlpPrice() {
         const pools = res.data.data;
         const provider = new ethers.providers.JsonRpcProvider(RPC); 
         for(const pool of pools) {
-            const {pair_address, tvl} = pool
-            const tokenContract = new ethers.Contract(pair_address, erc20Abi, provider);
-            const total = await tokenContract.totalSupply();
-            const balance = ethers.utils.formatUnits(total)
-            const price = tvl/balance || 0;
-            poolData[pair_address] = price
+            try {
+                const {pair_address, tvl} = pool
+                const tokenContract = new ethers.Contract(pair_address, erc20Abi, provider);
+                const total = await tokenContract.totalSupply();
+                const balance = ethers.utils.formatUnits(total)
+                const price = tvl/balance || 0;
+                poolData[pair_address] = price
+            } catch (error) {
+                console.error(error)
+            }
         }
     }
     fs.writeFileSync('./pools.json', JSON.stringify(poolData), 'utf-8')
